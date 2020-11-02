87e09f51b4575a9a451f898f7182ad001740b4a9
Grzegorz Ziemba <grzegorzziemba@Grzegorzs-MacBook-Pro.local>
2020-11-02 21:50:03 +0100

    forms styling

--- src/components/EditJobForm.js
+++ src/components/EditJobForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../firebase/firebase";
+import { Form, Button } from "react-bootstrap";
 
 const EditJobForm = () => {
 	let { jobId } = useParams();
@@ -8,7 +9,8 @@ const EditJobForm = () => {
 	const [position, setPosition] = useState("");
 	const [salary, setSalary] = useState("");
 
-	const editItem = () => {
+	const editItem = (e) => {
+		e.preventDefault();
 		db.collection("workplaces").doc(jobId).update({
 			work: work,
 			position: position,
@@ -18,23 +20,43 @@ const EditJobForm = () => {
 	};
 	return (
 		<div>
-			<input
-				placeholder={jobId}
-				value={work}
-				onChange={(e) => setWork(e.target.value)}
-			/>
-			<input
-				placeholder={jobId}
-				value={position}
-				onChange={(e) => setPosition(e.target.value)}
-			/>
-			<input
-				placeholder={jobId}
-				value={salary}
-				onChange={(e) => setSalary(e.target.value)}
-			/>
-			Wpisz cos
-			<button onClick={editItem}>Kliknij by zmienic</button>
+			<Form>
+				<Form.Group controlId="formBasicEmail">
+					<Form.Label>Company name</Form.Label>
+					<Form.Control
+						type="text"
+						placeholder="Company Name"
+						value={work}
+						onChange={(e) => setWork(e.target.value)}
+					/>
+					<Form.Text className="text-muted">
+						Your company name is very important for us, let your future employee
+						know who you are ! :)
+					</Form.Text>
+				</Form.Group>
+
+				<Form.Group controlId="formBasicPassword">
+					<Form.Label>Position</Form.Label>
+					<Form.Control
+						type="text"
+						placeholder="Who are you looking for ?"
+						value={position}
+						onChange={(e) => setPosition(e.target.value)}
+					/>
+				</Form.Group>
+				<Form.Group controlId="formBasicPassword">
+					<Form.Label>Salary</Form.Label>
+					<Form.Control
+						type="number"
+						placeholder="How much your future employee will earn?"
+						value={salary}
+						onChange={(e) => setSalary(e.target.value)}
+					/>
+				</Form.Group>
+				<Button variant="primary" type="submit" onClick={editItem}>
+					Submit
+				</Button>
+			</Form>
 		</div>
 	);
 };
