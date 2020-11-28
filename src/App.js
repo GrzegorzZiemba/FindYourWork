c2aedfa24cf8d0758c56c595731e5110551f77c5
GrzegorzZiemba <gzgrzegorzziemba@gmail.com>
2020-11-28 19:38:26 +0100

    Added new Forms, move Home

--- src/App.js
+++ src/App.js
@@ -44,6 +44,7 @@ class App extends Component {
 						</Switch>
 					</div>
 				</Router>
+				<Home />
 			</div>
 		);
 	}
--- src/components/EditJobForm.js
+++ src/components/EditJobForm.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../firebase/firebase";
 import { Form, Button } from "react-bootstrap";
+import { TextField } from "@material-ui/core";
 
 const EditJobForm = () => {
 	let { jobId } = useParams();
@@ -43,38 +44,32 @@ const EditJobForm = () => {
 	return (
 		<div>
 			<Form>
-				<Form.Group controlId="formBasicEmail">
-					<Form.Label>Company name</Form.Label>
-					<Form.Control
-						type="text"
-						placeholder={work}
-						value={work}
-						onChange={(e) => setWork(e.target.value)}
-					/>
-					<Form.Text className="text-muted">
-						Your company name is very important for us, let your future employee
-						know who you are ! :)
-					</Form.Text>
-				</Form.Group>
+				<TextField
+					id="standard-basic"
+					label="image"
+					type="text"
+					placeholder={work}
+					value={work}
+					onChange={(e) => setWork(e.target.value)}
+				/>
+				<TextField
+					id="standard-basic"
+					label="image"
+					type="text"
+					placeholder={position}
+					value={position}
+					onChange={(e) => setPosition(e.target.value)}
+				/>
 
-				<Form.Group controlId="formBasicPassword">
-					<Form.Label>Position</Form.Label>
-					<Form.Control
-						type="text"
-						placeholder={position}
-						value={position}
-						onChange={(e) => setPosition(e.target.value)}
-					/>
-				</Form.Group>
-				<Form.Group controlId="formBasicPassword">
-					<Form.Label>Salary</Form.Label>
-					<Form.Control
-						type="number"
-						placeholder={salary}
-						value={salary}
-						onChange={(e) => setSalary(e.target.value)}
-					/>
-				</Form.Group>
+				<TextField
+					id="standard-basic"
+					label="image"
+					type="number"
+					placeholder={salary}
+					value={salary}
+					onChange={(e) => setSalary(e.target.value)}
+				/>
+
 				<Button variant="primary" type="submit" onClick={editItem}>
 					Submit
 				</Button>
