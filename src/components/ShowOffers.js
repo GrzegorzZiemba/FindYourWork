3512e8adf72c6f3eb3adae11cb13dc7b19b97e2b
GrzegorzZiemba <gzgrzegorzziemba@gmail.com>
2020-11-29 15:29:25 +0100

    Adding some styling - changing hexagons to cards

--- src/components/ShowOffers.js
+++ src/components/ShowOffers.js
@@ -3,12 +3,28 @@ import { Button } from "react-bootstrap";
 import { db, fbase } from "../firebase/firebase";
 import { Link } from "react-router-dom";
 import DeleteData from "./DeleteData";
-import ImageHexa from "./ImageHexa";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
 import "./ShowOffers.css";
+import { makeStyles } from "@material-ui/core/styles";
 
 const auth = fbase.auth();
 
+const useStyles = makeStyles({
+	root: {
+		maxWidth: 345,
+	},
+	media: {
+		height: 140,
+	},
+});
+
 const ShowOffers = ({ image, id, workplace, position, styleClass, iden }) => {
+	const classes = useStyles();
 	const user = fbase.auth().currentUser;
 	let { uid } = auth.currentUser == null ? "" : auth.currentUser;
 	fbase.auth().onAuthStateChanged(function (user) {
@@ -24,7 +40,7 @@ const ShowOffers = ({ image, id, workplace, position, styleClass, iden }) => {
 	console.log(`iden ${iden}`);
 	return (
 		<div className="main">
-			<div className={styleClass}>
+			{/* <div className={styleClass}>
 				<span className="line"></span>
 
 				<ImageHexa
@@ -37,20 +53,38 @@ const ShowOffers = ({ image, id, workplace, position, styleClass, iden }) => {
 				<h1 className="workplace"> {workplace} </h1>
 				<h1 className="position" key={workplace}>
 					{position}
-				</h1>
+				</h1> */}
+
+			<Card className={classes.root}>
+				<CardActionArea>
+					<CardMedia
+						className={classes.media}
+						image={image}
+						title={workplace}
+					/>
+					<CardContent>
+						<Typography gutterBottom variant="h5" component="h2">
+							{workplace}
+						</Typography>
+						<Typography variant="body2" color="textSecondary" component="p">
+							{position}
+						</Typography>
+					</CardContent>
+				</CardActionArea>
+
 				{uid == iden && uid != "" ? (
-					<div className="buttons">
+					<CardActions>
 						<Link to={`/edit/${id}`}>
-							<Button style={{ background: "#BD7028 ", border: "none" }}>
+							<Button size="small" color="primary">
 								Edit
 							</Button>
 						</Link>
 						<DeleteData id={id} className="button" />
-					</div>
+					</CardActions>
 				) : (
 					<div>{console.log(uid, iden)}</div>
 				)}
-			</div>
+			</Card>
 		</div>
 	);
 };
