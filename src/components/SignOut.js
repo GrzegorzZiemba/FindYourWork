cafd5243bf1fc919b6c8345dde7a3bd7468cc878
GrzegorzZiemba <gzgrzegorzziemba@gmail.com>
2020-12-20 12:43:40 +0100

    Solved problem with logout

--- src/components/SignOut.js
+++ src/components/SignOut.js
@@ -1,13 +1,27 @@
 import "firebase/auth";
 import firebase from "firebase/app";
-
+import { Button } from "react-bootstrap";
+import "./EditJobForm.css";
+import { Link } from "react-router-dom";
 function SignOut() {
 	const auth = firebase.auth();
 	return (
 		auth.currentUser && (
-			<button className="sign-out" onClick={() => auth.signOut()}>
-				Sign Out
-			</button>
+			<div className="signOutCenter">
+				<h1>Are you sure u wanna sign out?</h1>
+				<div>
+					<a href="/">
+						<Button className="sign-out" onClick={() => auth.signOut()}>
+							Yes
+						</Button>
+					</a>
+					<Link to="/">
+						<Button className="sign-out" size="lg">
+							No
+						</Button>
+					</Link>
+				</div>
+			</div>
 		)
 	);
 }
