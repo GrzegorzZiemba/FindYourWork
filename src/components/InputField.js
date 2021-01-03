bc989ac1e036080be42258f6c5e89285981e5b2c
GrzegorzZiemba <gzgrzegorzziemba@gmail.com>
2021-01-03 15:23:49 +0100

    Added counter of letters

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./form.css";
 const InputField = ({ name, data, setChange, type, input }) => {
+	const [count, setCount] = useState("");
 	return (
-		<div class="form__div">
+		<div className="form__div">
 			{input ? (
 				<textarea
 					type="text"
 					placeholder={` ${data}`}
-					class="form__input"
-					onChange={(e) => setChange(e.target.value)}
+					className="form__input"
+					onChange={(e) => {
+						setCount(e.target.value.length);
+						setChange(e.target.value);
+					}}
 					value={data}
 					type={type ? type : "text"}
 				/>
@@ -16,16 +20,26 @@ const InputField = ({ name, data, setChange, type, input }) => {
 				<input
 					type="text"
 					placeholder={` ${data}`}
-					class="form__input"
-					onChange={(e) => setChange(e.target.value)}
+					className="form__input"
+					onChange={(e) => {
+						setCount(e.target.value.length);
+						if (count < input ? 500 : 50) {
+							setChange(e.target.value);
+						} else {
+							alert(`${data} is too long`);
+						}
+					}}
 					value={data}
 					type={type ? type : "text"}
 				/>
 			)}
 
-			<label for="" class="form__label">
+			<label for="" className="form__label">
 				{name}
 			</label>
+			<p className="form__paragraph">
+				{count} / {input ? 500 : 50}
+			</p>
 		</div>
 	);
 };
